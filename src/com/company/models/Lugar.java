f910f5e7b23e3fdf8f8ff6c9f6a2fe8d24ed1493
Gonzalo Larralde <gonzalolarralde@gmail.com>
2017-11-07 20:16:35 -0300

    Agrego primeros models.

---
+++ src/com/company/models/Lugar.java
@@ -0,0 +1,26 @@
+package com.company.models;
+
+public class Lugar {
+    private String nombre, descripcion;
+
+    public Lugar(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
---
+++ src/com/company/models/TablaDeDestinos.java
@@ -0,0 +1,63 @@
+package com.company.models;
+
+public class TablaDeDestinos {
+    private Lugar origen, destino;
+    private int costoMillas, gananciaMillas;
+
+    public TablaDeDestinos(Lugar origen, Lugar destino, int costoMillas, int gananciaMillas) {
+        this.origen = origen;
+        this.destino = destino;
+        this.costoMillas = costoMillas;
+        this.gananciaMillas = gananciaMillas;
+    }
+
+    public Lugar getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(Lugar origen) {
+        this.origen = origen;
+    }
+
+    public Lugar getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Lugar destino) {
+        this.destino = destino;
+    }
+
+    public int getCostoMillas() {
+        return costoMillas;
+    }
+
+    public void setCostoMillas(int costoMillas) {
+        this.costoMillas = costoMillas;
+    }
+
+    public int getGananciaMillas() {
+        return gananciaMillas;
+    }
+
+    public void setGananciaMillas(int gananciaMillas) {
+        this.gananciaMillas = gananciaMillas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TablaDeDestinos)) return false;
+
+        TablaDeDestinos that = (TablaDeDestinos) o;
+
+        if (!origen.equals(that.origen)) return false;
+        return destino.equals(that.destino);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = origen.hashCode();
+        result = 31 * result + destino.hashCode();
+        return result;
+    }
+}
