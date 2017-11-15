6e785a5232d5fdd8c2c125f03beb813e309cf8fc
Gonzalo Larralde <gonzalolarralde@gmail.com>
2017-11-14 22:25:31 -0300

    Agrego dependencia de Logging y primeros endpoints.
    El server levanta en localhost:8080. todavía no pude mappear el
    directorio estatico correctamente.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,21 @@ package com.company;
 import static spark.Spark.*;
 
 public class Main {
-
     public static void main(String[] args) {
+        org.apache.log4j.BasicConfigurator.configure();
+
+        port(8080);
+
+        staticFileLocation("../../../../web");
+
+        get("/api/lugares", (request, response) -> {
+            return "[{\"id\": 1, \"nombre\": \"Buenos Aires (EZE)\"}, {\"id\": 2, \"nombre\": \"Buenos Aires (AEP)\"}, {\"id\": 3, \"nombre\": \"Florianopolis (FLP)\"}]";
+        });
+
+        get("/api/tramos", (request, response) -> {
+            return "[{\"id\": 1, \"origen\": 1, \"destino\": 3, \"costo\": 500, \"ganancia\": 200}, {\"id\": 2, \"origen\": 2, \"destino\": 3, \"costo\": 450, \"ganancia\": 210}, {\"id\": 3, \"origen\": 3, \"destino\": 1, \"costo\": 500, \"ganancia\": 200}]";
+        });
+
 
     }
 }
